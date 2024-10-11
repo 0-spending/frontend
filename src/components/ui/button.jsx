7200882033f6aaf33318617fb860c31e87ca4497
jihyeoniiiii <105184159+Jihyeoniiiii@users.noreply.github.com>
2024-10-11 11:31:09 +0900

    refactor: apply theme and refactor props

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-  width: 261px;
-  height: 53px;
+  width: ${(props) => props.width || '261px'};
+  height: ${(props) => props.height || '53px'};
   padding: 16px;
-  gap: 10px;
-  background-color: ${(props) => props.color || props.theme.colors.primary[400]};
+  background-color: ${(props) => props.theme.colors.primary[props.variant] || props.theme.colors.primary[400]};
   border-radius: 8px;
   border-width: 1px;
   border-style: solid;
   border-color: ${(props) => props.borderColor || props.theme.colors.grayscale[1300]};
+  opacity: ${(props) => props.opacity || 1};
 `;
 
 export default Button;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme.js';
 
 import Home from './pages/home.jsx'
 
 const router = createBrowserRouter([
-  { path: '/', element: <Home /> }
+  { path: '/', element: <Home /> },
 ])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <ThemeProvider theme={theme}>
     <RouterProvider router={router} />
-  </StrictMode>
+  </ThemeProvider>
 )
