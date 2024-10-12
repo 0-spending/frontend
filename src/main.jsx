26c2d6898908c0e2e02354d63af5ff0b7ed93a66
jihyeoniiiii <105184159+Jihyeoniiiii@users.noreply.github.com>
2024-10-12 21:31:26 +0900

    feat: add modify profile page

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import { theme } from './styles/theme.js';
 
 // import Home from './pages/home.jsx'
 import WritePost from './pages/write-post.jsx';
+import ModifyProfile from './pages/modify_profile.jsx';
 
 const router = createBrowserRouter([
   // { path: '/', element: <Home /> },
   { path: '/write', element: <WritePost /> },
+  { path: '/profile/modify', element: <ModifyProfile />},
 ])
 
 createRoot(document.getElementById('root')).render(
