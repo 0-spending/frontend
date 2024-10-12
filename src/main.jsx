509fd2843dc139f545897ac9fa4a31c48561fad2
pongdang04 <your.pongdangdang419@gmail.com>
2024-10-12 16:18:49 +0900

    Feat: Update routes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,24 @@
-// src/main.jsx
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme.js';
+
+// import Home from './pages/home.jsx'
+import WritePost from './pages/write-post.jsx';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import ProfilePage from './pages/ProfilePage'; 
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  createRoot(rootElement).render(
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/profile" element={<ProfilePage />} /> 
-      </Routes>
-    </Router>
-  );
-}
+const router = createBrowserRouter([
+  // { path: '/', element: <Home /> },
+  { path: '/write', element: <WritePost /> },
+  { path: '/', element: <LoginPage /> },          
+  { path: '/signup', element: <SignupPage /> },   
+  { path: '/profile', element: <ProfilePage /> }, 
+])
+
+createRoot(document.getElementById('root')).render(
+  <ThemeProvider theme={theme}>
+    <RouterProvider router={router} />
+  </ThemeProvider>
+)
