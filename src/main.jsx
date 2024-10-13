3f78b3d31b20d9d75a09fb598ed4c9c4792c98ac
pongdang04 <your.pongdangdang419@gmail.com>
2024-10-13 09:20:44 +0900

    Update GNB and main.jsx with new layout

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import ProfilePage from './pages/ProfilePage'; 
+import GNB from './components/GNB';  
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <Router>
+      <GNB />  
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
@@ -18,3 +20,4 @@ if (rootElement) {
     </Router>
   );
 }
+
