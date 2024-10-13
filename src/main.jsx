5dcb2bf5b88c07efc73b79087ab2fd6f3b470712
pongdang04 <your.pongdangdang419@gmail.com>
2024-10-13 11:19:27 +0900

    Fix GNB to appear on all pages and update layout

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import ProfilePage from './pages/ProfilePage'; 
-import GNB from './components/GNB';  
+import GNB from './components/GNB';  // GNB 컴포넌트 임포트
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <Router>
-      <GNB />  
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/profile" element={<ProfilePage />} /> 
-      </Routes>
+      <div>
+        <GNB />  {/* 네비게이션 바를 모든 페이지에 추가 */}
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/profile" element={<ProfilePage />} /> 
+        </Routes>
+      </div>
     </Router>
   );
 }
