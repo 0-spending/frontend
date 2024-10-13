427f29db3e7cf83636f2a133ec3003bb99a7a6e3
pongdang04 <your.pongdangdang419@gmail.com>
2024-10-13 11:30:56 +0900

    Fix GNB to appear on all pages and update layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,34 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme.js';
+
+import WritePost from './pages/write-post.jsx';
+import ModifyProfile from './pages/modify_profile.jsx';
+import HomeGuest from './pages/home-guest.jsx';
+import HomeLogin from './pages/home-login.jsx';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import ProfilePage from './pages/ProfilePage'; 
-import GNB from './components/GNB';  // GNB 컴포넌트 임포트
+import GNB from './components/GNB';  // GNB 컴포넌트 추가
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  createRoot(rootElement).render(
-    <Router>
-      <div>
-        <GNB />  {/* 네비게이션 바를 모든 페이지에 추가 */}
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/profile" element={<ProfilePage />} /> 
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+const router = createBrowserRouter([
+  { path: '/', element: <HomeGuest /> },
+  { path: '/write', element: <WritePost /> },
+  { path: '/homelogin', element: <HomeLogin /> },
+  { path: '/login', element: <LoginPage /> },          
+  { path: '/signup', element: <SignupPage /> },   
+  { path: '/profile', element: <ProfilePage /> },
+  { path: '/profile/modify', element: <ModifyProfile /> },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <ThemeProvider theme={theme}>
+    <div>
+      <GNB />  {/* GNB 네비게이션 바를 모든 페이지에 추가 */}
+      <RouterProvider router={router} />
+    </div>
+  </ThemeProvider>
+);
 
